c2eb031bb26dc71f05871642c6d2d5a7259e90af
Rebeca Damas <damas.rebeca@outlook.com>
2023-05-26 11:03:16 -0300

    Variáveis e Operações

---
+++ teste.js
@@ -0,0 +1,3 @@
+// Estou imprimindo duas mensagens no terminal
+console.log('Hello World')
+console.log('Bem vinde ao cuirso de JavaScript')
---
+++ variaveis.js
@@ -0,0 +1,31 @@
+// Tipos de variáveis
+// Number
+let idade = 25
+let numero = 30
+let altura = 1.71
+// String
+let nome = 'Rebeca'
+// Boolean
+let estudando = true
+
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(idade, typeof idade)
+console.log(estudando, typeof estudando)
+
+// Undefined
+let outra;
+// Null
+let nula = null
+
+console.log(outra)
+console.log(nula)
+
+// Não utilizar o var e sim o let
+var variavel = 32
+// Constante
+const constante = 'sempre'
+
+console.log(constante)
