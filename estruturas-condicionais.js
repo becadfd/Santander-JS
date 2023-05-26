9df53fedacebd2253fad3c3b09e37771b2c0e5bf
Rebeca Damas <damas.rebeca@outlook.com>
2023-05-26 12:18:37 -0300

    Estruturas Condicionais

---
+++ estruturas-condicionais.js
@@ -0,0 +1,27 @@
+const idade = 20
+
+if(idade >= 18) {
+    console.log('Você é maior de idade')
+} else {   
+    console.log('Você é menor de idade')
+}
+
+
+// Se média >=7, aprovado
+// Se média < 7 e média >= 5, Recuperação
+// Se média < 5, Reprovado
+
+console.clear()
+
+let media = 4.99
+
+if (media >= 7){
+    console.log('Aprovado(a)')
+} else if (media >=5) {
+    console.log('Recuperação')
+} else {
+    console.log('Reprovado(a)')
+}
+
+
+
