3239e775e0fafb2e98fcf1d676e86ac77ee4c663
Rebeca Damas <damas.rebeca@outlook.com>
2023-05-26 11:24:08 -0300

    Operadores Aritméticos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,30 @@
+let primNumero  = 20
+let segNumero = 2
+
+// Adição: +
+// Subtração: -
+// Multiplicação: *
+// Divisão: /
+// Exponenciação: **
+// Resto da Divisão: %
+
+const soma = primNumero + segNumero
+
+console.log('Soma = ', soma)
+console.log('Subtração =', primNumero - segNumero)
+console.log('Multiplicação =', primNumero * segNumero)
+console.log('Divisão =', primNumero / segNumero)
+console.log('Esponenciação =', primNumero ** segNumero)
+console.log('Resto da Divisão =', primNumero % segNumero)
+
+// Precedência de Operadores
+// A multiplicação é feita primeiro
+console.log(3 + 7 * 3)
+
+// Usar o parênteses para escolher qual operação será realizada primeiro
+console.log((3 + 7) * 3)
+console.log((10 + 7 + 8) / 3)
+
+// Biblioteca Math
+console.log(Math.PI)
+console.log(Math.sqrt(16)) //raiz quadrada
